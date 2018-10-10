feb1c5c6268b393991e44d7497fb0eac37757ae6
Prashant Kumar Singh <prashant.siet153@gmail.com>
2018-10-10 11:39:10 +0530

    Added Spring boot Application Structure

---
+++ src/main/java/com/prashant/webcrawler/PrudentialWebcrawlerApplication.java
@@ -0,0 +1,19 @@
+package com.prashant.webcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * Spring Application Boostrapper
+ * 
+ * @author prasingh26
+ *
+ */
+@SpringBootApplication
+public class PrudentialWebcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrudentialWebcrawlerApplication.class, args);
+	}
+}
